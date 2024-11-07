ffa0287ae68e3e2525b11e1115599351d5d20bdb
jasserayadi <86734763+jasserayadi@users.noreply.github.com>
2024-11-07 03:25:40 +0100

    7comimt

--- src/main/java/tn/esprit/tpfoyer17/services/impementations/ChambreService.java
+++ src/main/java/tn/esprit/tpfoyer17/services/impementations/ChambreService.java
@@ -54,8 +54,7 @@ public class ChambreService implements IChambreService {
     @Override
     public Bloc affecterChambresABloc(List<Long> numChambre, long idBloc) {
        Bloc bloc = blocRepository.findById(idBloc).orElse(null);
-       //List<Chambre> chambreList = (List<Chambre>) chambreRepository.findAllById(numChambre);
-       List<Chambre> chambreList =  chambreRepository.findByNumeroChambreIn(numChambre);
+          List<Chambre> chambreList =  chambreRepository.findByNumeroChambreIn(numChambre);
 
        for(Chambre chambre: chambreList) {
            chambre.setBloc(bloc);
