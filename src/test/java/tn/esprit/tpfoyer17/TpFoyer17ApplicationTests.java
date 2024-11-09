f5f0e1680b27b0e9647d32b2749cf6d3784b316f
Eya Mrad <eya.mrad@esprit.tn>
2024-11-09 14:36:09 +0100

    second commit13

--- src/test/java/tn/esprit/tpfoyer17/TpFoyer17ApplicationTests.java
+++ src/test/java/tn/esprit/tpfoyer17/TpFoyer17ApplicationTests.java
@@ -1,9 +1,10 @@
 package tn.esprit.tpfoyer17;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
 
 
-
+@SpringBootTest
 class TpFoyer17ApplicationTests {
 
     @Test
