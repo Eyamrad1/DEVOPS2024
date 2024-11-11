62ef3f0bee76867533d9d65e361839cf1fdc1a86
aminelahmer1 <aminelahmer999@hotmail.com>
2024-11-11 17:43:19 +0000

    H2database2

--- src/test/java/tn/esprit/tpfoyer17/ChambreServiceTestMockito.java
+++ src/test/java/tn/esprit/tpfoyer17/ChambreServiceTestMockito.java
@@ -10,6 +10,7 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import tn.esprit.tpfoyer17.entities.Bloc;
 import tn.esprit.tpfoyer17.entities.Chambre;
 import tn.esprit.tpfoyer17.entities.enumerations.TypeChambre;
@@ -23,8 +24,9 @@ import java.util.Optional;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
-
-
+@ActiveProfiles("test")
+@SpringBootTest
+@ExtendWith(MockitoExtension.class)
 public class ChambreServiceTestMockito {
 
     @Mock
